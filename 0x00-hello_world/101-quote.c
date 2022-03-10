33830ef94f35eb2f226c3c023c5ffa33d3799daa
root <annatefera3@gmail.com>
2022-03-10 07:47:53 -0800

    adding file that prints and that piece of art is useful - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
-   * main - prints to the standard error
-   *
-   * Return: one
-   */
+ *main - Entry point
+ *Return: Always 1
+ *
+ */
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
