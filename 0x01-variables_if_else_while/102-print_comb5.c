5af069bc2ae3b0048d80cca54efdb35a7c7aa67e
root <annatefera3@gmail.com>
2022-03-11 07:48:21 -0800

    adding file that prints two two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - print 00 to 99
- *
- * Return: zero
- */
+* main -prints two two digit 
+*
+* Return: 0
+*/
 int main(void)
 {
-	int i;
+int a, b;
 
-	for (i = 0; i <= 99; i++)
-	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i == 99)
-			break;
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+for (a = 0; a < 100; a++)
+{
+for (b = 0; b < 100; b++)
+{
+if (a < b && a != b)
+{
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar ((b / 10) + '0');
+putchar ((b % 10) + '0');
+if (!(a == 98 && b == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
 }
