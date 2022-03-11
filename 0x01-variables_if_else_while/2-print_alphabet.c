b939f8c957438230898c4d9ebfb55542b76ab806
root <annatefera3@gmail.com>
2022-03-11 04:41:06 -0800

    adding file that prints the alphabet in lowercase using putchar function

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,8 +9,10 @@ int main(void)
 {
 	char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++){
-		putchar(ch);}	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}	
 	putchar('\n');
 	return (0);
 }
