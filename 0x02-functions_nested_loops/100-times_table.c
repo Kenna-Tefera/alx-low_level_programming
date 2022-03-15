5eaef2b907a6eac684b159f12c9b338c5aae6510
kena <annatefera3@gmail.com>
2022-03-16 01:08:37 +0300

    adding file that prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,47 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the times table
- * @n: takes in an integer
+ * print_times_table - prints the n times table, starting with 0
+ * n: size of time table
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+ int a,b,c
 
-	if (n > 15 || n < 0)
-		return;
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			k = i * j;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-			else if (k <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10 && k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(k / 10 % 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
+ if (n >= 0 && n <= 15)
+ {
+  for (a = 0; a <= n; a++)
+  {
+   for (b = 0; b <= n; b++)
+   {
+    c = a * b;
+    if (b == 0)
+    {
+     _putchar(c + '0');
+    } else if (c < 10 && b != 0)
+    {
+     _putchar(',');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar(c + '0');
+    } else if (c >= 10 && c < 100)
+    {
+     _putchar(',');
+     _putchar(' ');
+     _putchar(' ');
+     _putchar((c / 10) + '0');
+     _putchar((c % 10) + '0');
+    } else if (c >= 100)
+    {
+     _putchar(',');
+     _putchar(' ');
+     _putchar((c / 100) + '0');
+     _putchar(((c / 10) % 10) + '0');
+     _putchar((c % 10) + '0');
+    }
+   }
+   _putchar('\n');
+  }
+ }
 }
