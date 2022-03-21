0ab7eeb9871f9f405feff47f637bb0265ca99d81
kena <annatefera3@gmail.com>
2022-03-21 13:17:05 +0300

    adding file that swaps the value of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: first integer.
+ * @b: second integer.
+ */
+void swap_int(int *a, int *b)
+{
+	int x = a*;
+	*a = *b;
+	*b = x;
+}
