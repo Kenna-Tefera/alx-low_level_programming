c39c2e8a8b1c29807d7368e262897281225ce8da
kena <annatefera3@gmail.com>
2022-03-21 12:54:27 +0300

    adding file that takes pointer and update the value it pointes to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ * the value in points to to 98.
+ *
+ * @n: pointer to n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
