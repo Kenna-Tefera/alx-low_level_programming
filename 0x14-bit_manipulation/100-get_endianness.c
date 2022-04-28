03c2ff08d9197f15b96c87d9aebb6586bb8069cc
Kenna-Tefera <annatefera3@gmail.com>
2022-04-28 15:23:58 +0300

    adding all files

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,19 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
-  * get_endianness - checks endianess.
-  * Return: 0 if big endian, 1 if little endian.
-  */
+ * get_endianness - checks the endiannes
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
 int get_endianness(void)
 {
-	int i = 1;
-	char *ch = (char *)&i;
+	int mask;
 
-	if (*ch)
+	mask = 1;
+
+	if ((mask >> 31) & 1)
+		return (0);
+	else
 		return (1);
-	return (0);
 }
