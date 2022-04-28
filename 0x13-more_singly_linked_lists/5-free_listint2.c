03548c8ace0f54ceae46f64af4e42b8a22ffab6a
Kenna-Tefera <annatefera3@gmail.com>
2022-04-28 08:18:17 +0300

    adding all files

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,21 @@
-#include <stdlib.h>
 #include "lists.h"
-
 /**
- * free_listint2 - frees a listint_t list.
- *
- * @head: a pointer to a pointer to head.
- * Return: nothing.
- */
+  * free_listint2 - frees a listint_t list and sets head to null.
+  * @head: pointer to pointer of first element of list.
+  *
+  * Return: void.
+  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *node;
+	listint_t *temp;
 
-	if (*head == NULL)
-		return;
-	node  = *head;
-	while (node != NULL)
+	if (head != NULL)
 	{
-		temp = node->next;
-		free(node);
-		node = temp;
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
-	*head = NULL;
 }
-
