b6e2b53a63d55c219e9263b1d29742151111e277
Kenna-Tefera <annatefera3@gmail.com>
2022-04-28 15:19:27 +0300

    adding all files

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,21 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
- *
- * @n: address of n
- * @index: position
- * Return: 1 if it worked, or -1 if an error occurred.
+ * set_bit - returns the value of a bit at a given index
+ * @n: decimal number
+ * @index: index
+ * Return: 1 if it worked or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	unsigned long int var, mask;
+
+	var = 0;
+	mask = 1;
+	if (index > 63)
 		return (-1);
-	*n ^= (1 << index);
+	var = *n | (mask << index);
+	*n = var;
 	return (1);
 }
